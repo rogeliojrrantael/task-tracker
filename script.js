318ccaa2dfa4a4772ca838a1b5281382482cf6ee
Rogelio Jr Rantael <rogeliojr.rantael@gmail.com>
2026-04-24 12:35:09 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+function addTask() {
+  const input = document.getElementById("taskInput");
+  const taskText = input.value;
+
+  if (taskText === "") return;
+
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  li.onclick = function () {
+    li.classList.toggle("done");
+  };
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "X";
+
+  deleteBtn.onclick = function () {
+    li.remove();
+  };
+
+  li.appendChild(deleteBtn);
+  document.getElementById("taskList").appendChild(li);
+
+  input.value = "";
+}
